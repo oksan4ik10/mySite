c1976d29529041c2c513fcf9c4597f9b17c83599
kalinina Oksana <ksan4ik92@gmail.com>
2022-02-08 13:28:26 +0300

    add modal and block main

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 const menuMobile = document.querySelector('.menu-mobile'),
 menu = document.querySelector('.menu'),
-menuMobileLine = document.querySelector('.menu-mobile__line');
+menuMobileLine = document.querySelector('.menu-mobile__line'),
+modal = document.querySelector('.modal'),
+modalClose = document.querySelector('.modal__close'),
+headerBtn = document.querySelector('.header__btn');
 
+//открытие меню для мобилки
 menuMobile.addEventListener('click', ()=>{
     menuMobileLine.classList.toggle('close');
     menu.classList.toggle('menu__open');
+})
+
+
+//открытие модального окна по кнопке Связаться
+headerBtn.addEventListener('click',()=>{
+    modal.style.display = 'flex';
+})
+modalClose.addEventListener('click',()=>{
+    modal.style.display = 'none';
 })
